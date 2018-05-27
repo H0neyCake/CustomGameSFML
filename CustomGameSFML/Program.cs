cacdc175140b6afe32608dcd262aa708e695076b
Cake <dimex4@ya.ru>
2018-05-28 00:28:31 +0300

    Initial commit.

---
+++ CustomGameSFML/Program.cs
@@ -0,0 +1,36 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGameSFML
+{
+    class Program
+    {
+        static RenderWindow win;
+
+        static void Main(string[] args)
+        {
+            RenderWindow win = new RenderWindow(new SFML.Window.VideoMode(800, 600), "Custom Game");
+            win.SetVerticalSyncEnabled(true);
+
+            win.Closed += Win_Closed;
+            
+            while (win.IsOpen)
+            {
+                win.DispatchEvents();
+
+                win.Clear(Color.Black);
+
+                win.Display();
+            }
+        }
+
+        private static void Win_Closed(object sender, EventArgs e)
+        {
+            win.Close();
+        }
+    }
+}
